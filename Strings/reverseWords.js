fa6f885e23f89b9675eb6885ad4ce9064b499898
Zammad Nasir <zammadnasir@gmail.com>
2026-04-17 21:45:02 +0500

    reverseWords-155

---
+++ Strings/reverseWords.js
@@ -0,0 +1,9 @@
+// 155
+
+var reverseWords = function (s) {
+  return s.trim().split(/\s+/).reverse().join(" ");
+};
+
+console.log(reverseWords("the sky is blue"));
+console.log(reverseWords("  hello world  "));
+console.log(reverseWords("a good   example"));
